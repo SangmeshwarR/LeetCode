173d27126275c77eec2decab58a0b6516bd0f2f6
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-06-15 06:59:38 +0000

    .

---
+++ MergeTwoSortedLists.py
@@ -0,0 +1,17 @@
+# 21. Merge Two Sorted Lists
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: # pyright: ignore[reportUndefinedVariable]
+        cur = dummy = ListNode() # pyright: ignore[reportUndefinedVariable]
+        while list1 and list2:               
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2, cur = list2.next, list2
+                
+        if list1 or list2:
+            cur.next = list1 if list1 else list2
+            
+        return dummy.next
