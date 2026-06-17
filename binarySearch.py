aa0189351eec85635f742b5c93e685895d9d3733
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-06-17 06:14:33 +0000

    ...

---
+++ binarySearch.py
@@ -0,0 +1,14 @@
+# 704. Binary Search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int: # type: ignore
+        l,h = 0, len(nums) - 1
+        while(l<=h):
+            m = (l+h)//2
+            if(nums[m] == target):
+                return m
+            elif(nums[m]<target):
+                l = m + 1
+            else:
+                h = m - 1
+        return -1
