2327df9667126a0900d7495983b2fd065a306646
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-06-20 02:58:10 +0000

    ...

---
+++ add2Num.py
@@ -0,0 +1,30 @@
+# 2.Add two numbers
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]: # pyright: ignore[reportUndefinedVariable]
+        dummy = ListNode() # type: ignore
+        res = dummy
+
+        total = carry = 0
+
+        while l1 or l2 or carry:
+            total = carry
+
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            
+            num = total % 10
+            carry = total // 10
+            dummy.next = ListNode(num) # pyright: ignore[reportUndefinedVariable]
+            dummy = dummy.next
+        
+        return res.next
--- plusOne.py
+++ plusOne.py
@@ -1,7 +1,7 @@
 # 66. Plus One
 
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits: List[int]) -> List[int]: # type: ignore
         carry = 1
         for i in range(len(digits) - 1, -1, -1):
             s = digits[i] + carry
