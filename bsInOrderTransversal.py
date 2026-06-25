2861db74fe794c95c94510a7e9a96d4f96ffce84
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-06-25 15:40:08 +0000

    plus  5

---
+++ bsInOrderTransversal.py
@@ -0,0 +1,15 @@
+# 94. Binary Tree Inorder Traversal
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+        return res
