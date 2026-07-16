49a0c194774f81570606300f6bc0e4baf6faf733
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-07-16 16:40:10 +0000

    ...

---
+++ Solutions/addDigits.py
@@ -0,0 +1,11 @@
+# 258. Add Digits
+
+class Solution(object):
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        elif num % 9 == 0:
+            return 9
+        else:
+            return num % 9
+        
