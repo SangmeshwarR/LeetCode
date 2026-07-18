d24a51568ee19007b2b41d86699e27f0a25a2b92
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-07-18 06:42:40 +0000

    ...

---
+++ Solutions/firstBadVersion.py
@@ -0,0 +1,12 @@
+# 278. First Bad Version
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l,r = 1,n
+        while l < r:
+            mid = l + (r-l)//2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
