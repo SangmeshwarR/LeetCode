6014af10fcb771754876690f1e0effcea2d4eb19
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-07-23 16:27:38 +0000

    ...

---
+++ Solutions/revString.py
@@ -0,0 +1,15 @@
+# 344. Reverse String
+
+class Solution:
+    def reverseString(self, s):
+
+        start = 0
+        end = len(s) - 1
+
+        while start <= end:
+
+            s[start], s[end] = s[end], s[start]
+
+            start += 1
+            end -= 1
+
