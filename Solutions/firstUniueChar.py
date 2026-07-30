9286f91ca41113e584e3fb9eb989ea196420434b
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-07-30 12:52:25 +0000

    ...

---
+++ Solutions/firstUniueChar.py
@@ -0,0 +1,16 @@
+# 387. First Unique Character in a String
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        count = {}
+        for char in s:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+
+        for i in range(len(s)):
+            if count[s[i]] == 1:
+                return i
+
+        return -1
