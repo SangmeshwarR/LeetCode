f5c2cba18c89329dd4baf2736a473406813f1f6c
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-08-07 04:07:51 +0000

    ...

---
+++ Solutions/convertNumToHex.py
@@ -0,0 +1,14 @@
+# 405. Convert a Number to Hexadecimal
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num  == 0:
+            return "0"
+        elif num < 0:
+            num += 2**32
+        h = "0123456789abcdef"
+        r = []
+        while num:
+            r.append(h[num & 15])
+            num //= 16
+        return "".join(r[::-1])
