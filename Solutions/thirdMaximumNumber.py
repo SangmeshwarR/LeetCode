83da3e034588ecda657f2d751ae3d9382179fc9b
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-08-13 16:18:00 +0000

    ...

---
+++ Solutions/thirdMaximumNumber.py
@@ -0,0 +1,8 @@
+# 414. Third Maximum Number
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        if len(set(nums)) >= 3:
+            return sorted(set(nums))[-3]
+        else:
+            return sorted(set(nums))[-1]
