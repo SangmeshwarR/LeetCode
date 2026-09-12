0681bc6d5dcb188a5adc4da158f630eb54231268
Sangmeshwar <167839122+SangmeshwarR@users.noreply.github.com>
2026-09-12 04:23:55 +0000

    ...

---
+++ Solutions/base7.py
@@ -0,0 +1,13 @@
+# 504. Base 7
+
+class Solution(object):
+    def convertToBase7(self, num):
+        if num == 0:
+            return "0"
+        sign = "-" if num < 0 else ""
+        num = abs(num)
+        result = ""
+        while num:
+            result = str(num % 7) + result
+            num //= 7
+        return sign + result
